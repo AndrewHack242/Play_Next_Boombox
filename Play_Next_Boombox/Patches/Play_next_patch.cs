7cc53018ef4a4d397c2d4f5ad46342dcdf553e35
AndrewHack242 <andrew.hack242@gmail.com>
2024-04-25 20:48:57 -0600

    Add project files.

---
+++ Play_Next_Boombox/Patches/Play_next_patch.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace Play_Next_Boombox.Patches
+{
+    [HarmonyPatch(typeof(BoomboxItem))]
+    internal class Play_next_patch
+    {
+
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+        static void Patch_start(ref AudioSource ___boomboxAudio)
+        {
+            ___boomboxAudio.loop = false;
+        }
+
+        [HarmonyPatch("Update")]
+        [HarmonyPrefix]
+        static void Patch_update(ref BoomboxItem __instance, ref AudioSource ___boomboxAudio, ref bool ___isPlayingMusic)
+        {
+            if (___isPlayingMusic && !___boomboxAudio.isPlaying)
+            {
+                __instance.ItemActivate(false);
+                __instance.ItemActivate(true);
+            }
+        }
+
+    }
+}
